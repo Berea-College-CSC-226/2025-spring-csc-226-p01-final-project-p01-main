77aff05a10bf6524867f36d2190d6141f1a9c45f
watta <watta@berea.edu>
2025-04-06 15:23:38 -0400

    File setup for classes and test suite

---
+++ quizbrain.py
@@ -0,0 +1,12 @@
+class QuizBrain:
+    def __init__(self):
+        pass
+
+    def still_has_question(self):
+        pass
+
+    def next_question(self):
+        pass
+
+    def check_answer(self):
+        pass
