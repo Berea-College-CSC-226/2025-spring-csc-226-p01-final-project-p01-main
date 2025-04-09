75c4d4137bdb99d2700411c3a372cffdc2c58e1b
conwelld <conwelld@berea.edu>
2025-04-09 10:23:23 -0400

    worked on code

---
+++ final_game/final_game_test_suite.py
@@ -0,0 +1,20 @@
+from draft1 import Rocket
+from inspect import getframeinfo, stack
+
+def unittest(did_pass):
+    """
+    Print the result of a unit test.
+    :param did_pass: a boolean representing the test
+    :return: None
+    """
+
+    caller = getframeinfo(stack()[1][0])
+    linenum = caller.lineno
+    if did_pass:
+        msg = "Test at line {0} ok.".format(linenum)
+    else:
+        msg = ("Test at line {0} FAILED.".format(linenum))
+    print(msg)
+
+def code_test():
+    Rocket.move()
