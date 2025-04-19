5513e2a69d2df1e1db9572f191bb5e2153bb9b88
Faryal <fatimaf@berea.edu>
2025-04-19 11:11:44 -0400

    Add TurtlePlayer class with move_left and move_right methods

--- Game Code/Baby, Steps.py
+++ Game Code/Baby, Steps.py
@@ -2,6 +2,23 @@
 
 import turtle
 
+class TurtlePlayer:
+    #setting up the turtle shape, color and position
+    def __init__(self):
+        self.turtle = turtle.Turtle()
+        self.turtle.shape("turtle")
+        self.turtle.penup()
+        self.turtle.goto(0, -200)
+
+    def move_left(self):
+        x = self.turtle.xcor()
+        self.turtle.setx(x - 30)
+
+    def move_right(self):
+        x = self.turtle.xcor()
+        self.turtle.setx(x + 30)
+
+
 def main():
     screen = turtle.Screen()
     screen.title("Turtle Catch Game")
