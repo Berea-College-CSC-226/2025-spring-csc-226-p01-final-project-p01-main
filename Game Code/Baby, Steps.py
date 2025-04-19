cfd7f5ab1b5d6a0fc5dd8ced437852d3f5066d66
Faryal <fatimaf@berea.edu>
2025-04-19 11:01:02 -0400

    Initial setup: created game window with turtle screen

--- Game Code/Baby, Steps.py
+++ Game Code/Baby, Steps.py
@@ -1,12 +1,15 @@
+# Step 1, setting up the background
+
 import turtle
 
 def main():
     screen = turtle.Screen()
     screen.title("Turtle Catch Game")
-    screen.bgcolor("lightblue")
+    screen.bgcolor("lightgreen")
     screen.setup(width=600, height=600)
 
     turtle.done()
 
 if __name__ == "__main__":
     main()
+
