94e3dfb5573df5d827878f51e19e16ff828356ee
ismailovak <ismailovak@berea.edu>
2025-04-21 22:07:05 -0400

    Completed test suites in the correct way by rearranging the code and adding "if __name__ == "__main__"" on line 52, also used "dataclass" on line 3-6 (explained by the TA) to successfully conduct unit test. Also, integrated the parts Dayton added with the new button and changing the width of the window and some planet coordinates.

--- final_game/finalcode.py
+++ final_game/finalcode.py
@@ -1,37 +1,9 @@
 import pygame
 import sys
-
-## <<<<<<< conwelld
-# Setup
-# pygame.init()
-# WIDTH, HEIGHT = 1000, 300
-# screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# pygame.display.set_caption("Rocket Lands on Planets")
-
-# # Load Images
-# WIDTH, HEIGHT = 1000, 189
-# background = pygame.image.load("planets.gif").convert_alpha()
-# background = pygame.transform.scale(background, (WIDTH, HEIGHT))
-
-# WIDTH, HEIGHT = 1000, 300
-# rocket_img = pygame.image.load("rocket.png").convert_alpha()
-# rocket_img = pygame.transform.scale(rocket_img, (30, 30))
-# rocket_img.set_colorkey((255, 255, 255))
-##=======
 from dataclasses import dataclass
 
-
-color = (255,255,255)
-color_light = (170,170,170)
-color_dark = (100,100,100)
-width = screen.get_width()
-height = screen.get_height()
-mouse = pygame.mouse.get_pos()
-smallfont = pygame.font.SysFont('Corbel', 35)
-text = smallfont.render('land', True, color)
-
-
 # Planet class
+# dataclass was explained by TA (Nauka) for unit test file in order to test "check_planet" method
 @dataclass
 class Planet:
     def __init__(self, name, description, x_start, x_end):
@@ -46,21 +18,6 @@ class Planet:
 
     def get_info(self):
         return f"{self.name}: {self.description}"
-
-# <<<<<<< conwelld
-# planet_zones = [
-#     Planet("Venus", "Second planet from the Sun.", 200, 260),
-#     Planet("Mercury", "Closest to the Sun.", 265, 305),
-#     Planet("Earth", "Our home planet.", 340, 380),
-#     Planet("Mars", "The Red Planet.", 430, 460),
-#     Planet("Jupiter", "The largest planet.", 470, 560),
-#     Planet("Saturn", "Famous for its rings.", 600, 670),
-#     Planet("Uranus", "Has a tilted rotation.", 710, 770),
-#     Planet("Neptune", "Furthest from the Sun.", 790, 860),
-# ]
-
-# # Rocket
-# =======
 
 
 class Rocket:
@@ -86,51 +43,8 @@ class Rocket:
                 self.current_planet = planet
                 return planet
 
-# <<<<<<< conwelld
-# running = True
-# while running:
-#     screen.blit(background, (0, 0))
-
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-
-#         if event.type == pygame.KEYDOWN:
-#             if event.key == pygame.K_RIGHT: #Moves the rocket to the right by using the key "right"
-#                 rocket.move_right()
-#             elif event.key == pygame.K_r: #Key "R" sets the rocket to the start
-#                 rocket.reset()
-#                 for p in planet_zones:
-#                     p.hit = False
-#         if event.type == pygame.MOUSEBUTTONDOWN:
-#             if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
-#             # open new screen
-# =======
             if planet.is_hit(self.x) and not planet.hit:
-
                 self.current_planet = planet
-
-# <<<<<<< conwelld
-#     if rocket.current_planet:
-#         pygame.draw.rect(screen, (0, 0, 0), (20, 20, 500, 40))
-#         info_text = font.render(rocket.current_planet.get_info(), True, (255, 255, 255))
-#         screen.blit(info_text, (30, 30))
-#         if width/2.5 <= mouse[0] <= width/2+140 and height/1.58 <= mouse[1] <= height/1.58+30:
-#             pygame.draw.rect(screen, color_light, [width / 2.5, height / 1.58, 140, 40])
-#         else:
-#             pygame.draw.rect(screen, color_dark, [width / 2.5, height / 1.58, 140, 40])
-
-#         screen.blit(text, (width / 2.5 + 40, height / 1.58))
-#     pygame.display.update()
-#     clock.tick(30)
-
-# pygame.quit()
-# sys.exit()
-
-
-# '''important note *** when we integrate a button we should have the event handler for text, we just have a button pop up
-# when a planet is hit then we handle screen and changing them'''
-# =======
                 return planet
         return None
 
@@ -138,9 +52,18 @@ class Rocket:
 if __name__ == "__main__":
     # Setup
     pygame.init()
-    WIDTH, HEIGHT = 1000, 300
+    WIDTH, HEIGHT = 1000, 189
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     pygame.display.set_caption("Rocket Lands on Planets")
+
+    color = (255, 255, 255)
+    color_light = (170, 170, 170)
+    color_dark = (100, 100, 100)
+    width = screen.get_width()
+    height = screen.get_height()
+    mouse = pygame.mouse.get_pos()
+    smallfont = pygame.font.SysFont('Corbel', 35)
+    text = smallfont.render('land', True, color)
 
     # Load Images
     background = pygame.image.load("planets.gif").convert_alpha()
@@ -152,12 +75,12 @@ if __name__ == "__main__":
 
     planet_zones = [
         Planet("Venus", "Second planet from the Sun.", 200, 260),
-        Planet("Mercury", "Closest to the Sun.", 270, 305),
-        Planet("Earth", "Our home planet.", 310, 380),
-        Planet("Mars", "The Red Planet.", 400, 460),
+        Planet("Mercury", "Closest to the Sun.", 265, 305),
+        Planet("Earth", "Our home planet.", 340, 380),
+        Planet("Mars", "The Red Planet.", 430, 460),
         Planet("Jupiter", "The largest planet.", 470, 560),
-        Planet("Saturn", "Famous for its rings.", 580, 670),
-        Planet("Uranus", "Has a tilted rotation.", 680, 770),
+        Planet("Saturn", "Famous for its rings.", 600, 670),
+        Planet("Uranus", "Has a tilted rotation.", 710, 770),
         Planet("Neptune", "Furthest from the Sun.", 790, 860),
     ]
 
@@ -184,6 +107,9 @@ if __name__ == "__main__":
                     rocket.reset()
                     for p in planet_zones:
                         p.hit = False
+            # if event.type == pygame.MOUSEBUTTONDOWN:
+                # if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
+                # open new screen
 
         found_planet = rocket.check_planet(planet_zones)
         rocket.draw()
@@ -192,6 +118,12 @@ if __name__ == "__main__":
             pygame.draw.rect(screen, (0, 0, 0), (20, 20, 500, 40))
             info_text = font.render(rocket.current_planet.get_info(), True, (255, 255, 255))
             screen.blit(info_text, (30, 30))
+            if width / 2.5 <= mouse[0] <= width / 2 + 140 and height / 1.58 <= mouse[1] <= height / 1.58 + 30:
+                pygame.draw.rect(screen, color_light, [width / 2.5, height / 1.58, 140, 40])
+            else:
+                pygame.draw.rect(screen, color_dark, [width / 2.5, height / 1.58, 140, 40])
+
+            screen.blit(text, (width / 2.5 + 40, height / 1.58))
 
         pygame.display.update()
         clock.tick(30)
--- final_game/test_game_logic.py
+++ final_game/test_game_logic.py
@@ -19,14 +19,14 @@ def unittest(did_pass):
 
 
 def genomics_test_suite():
-    venus = Planet("Venus", "Second venus from the Sun.", 200, 260)
-    mercury = Planet("Mercury", "Closest to the Sun.", 270, 305)
-    earth = Planet("Earth", "Our home venus.", 310, 380)
-    Planet("Mars", "The Red Planet.", 400, 460)
-    Planet("Jupiter", "The largest venus.", 470, 560)
-    Planet("Saturn", "Famous for its rings.", 580, 670)
-    Planet("Uranus", "Has a tilted rotation.", 680, 770)
-    Planet("Neptune", "Furthest from the Sun.", 790, 860)
+    venus = Planet("Venus", "Second planet from the Sun.", 200, 260)
+    mercury = Planet("Mercury", "Closest to the Sun.", 265, 305)
+    earth = Planet("Earth", "Our home planet.", 340, 380)
+    mars = Planet("Mars", "The Red Planet.", 430, 460)
+    jupiter = Planet("Jupiter", "The largest planet.", 470, 560)
+    saturn = Planet("Saturn", "Famous for its rings.", 600, 670)
+    uranus = Planet("Uranus", "Has a tilted rotation.", 710, 770)
+    neptune = Planet("Neptune", "Furthest from the Sun.", 790, 860)
 
     # Testing Venus
     unittest(venus.is_hit(200) == True)
@@ -39,20 +39,65 @@ def genomics_test_suite():
     unittest(mercury.is_hit(280) == True)
     unittest(mercury.is_hit(300) == True)
     unittest(mercury.is_hit(305) == True)
+    unittest(mercury.is_hit(265) == True)
+    unittest(mercury.is_hit(260) == False)
+
+    # Testing Earth
+    unittest(earth.is_hit(340) == True)
+    unittest(earth.is_hit(360) == True)
+    unittest(earth.is_hit(380) == True)
+    unittest(earth.is_hit(339) == False)
+
+    # Testing Mars
+    unittest(mars.is_hit(430) == True)
+    unittest(mars.is_hit(445) == True)
+    unittest(mars.is_hit(460) == True)
+    unittest(mars.is_hit(461) == False)
+
+    # Testing Jupiter
+    unittest(jupiter.is_hit(470) == True)
+    unittest(jupiter.is_hit(500) == True)
+    unittest(jupiter.is_hit(560) == True)
+    unittest(jupiter.is_hit(561) == False)
+
+    # Testing Saturn
+    unittest(saturn.is_hit(600) == True)
+    unittest(saturn.is_hit(635) == True)
+    unittest(saturn.is_hit(670) == True)
+    unittest(saturn.is_hit(680) == False)
+
+    # Testing Uranus
+    unittest(uranus.is_hit(710) == True)
+    unittest(uranus.is_hit(740) == True)
+    unittest(uranus.is_hit(770) == True)
+    unittest(uranus.is_hit(709) == False)
+
+    # Testing Neptune
+    unittest(neptune.is_hit(790) == True)
+    unittest(neptune.is_hit(825) == True)
+    unittest(neptune.is_hit(860) == True)
+    unittest(neptune.is_hit(861) == False)
 
     rocket = Rocket()
     planet_zones = [
-        Planet("Venus", "Second venus from the Sun.", 200, 260),
-        Planet("Mercury", "Closest to the Sun.", 270, 305),
-        Planet("Earth", "Our home venus.", 310, 380),
-        Planet("Mars", "The Red Planet.", 400, 460),
-        Planet("Jupiter", "The largest venus.", 470, 560),
-        Planet("Saturn", "Famous for its rings.", 580, 670),
-        Planet("Uranus", "Has a tilted rotation.", 680, 770),
+        Planet("Venus", "Second planet from the Sun.", 200, 260),
+        Planet("Mercury", "Closest to the Sun.", 265, 305),
+        Planet("Earth", "Our home planet.", 340, 380),
+        Planet("Mars", "The Red Planet.", 430, 460),
+        Planet("Jupiter", "The largest planet.", 470, 560),
+        Planet("Saturn", "Famous for its rings.", 600, 670),
+        Planet("Uranus", "Has a tilted rotation.", 710, 770),
         Planet("Neptune", "Furthest from the Sun.", 790, 860),
     ]
     unittest(rocket.check_planet(planet_zones, testing=True) == venus)
     unittest(rocket.check_planet(planet_zones, testing=True) == mercury)
+    unittest(rocket.check_planet(planet_zones, testing=True) == earth)
+    unittest(rocket.check_planet(planet_zones, testing=True) == mars)
+    unittest(rocket.check_planet(planet_zones, testing=True) == jupiter)
+    unittest(rocket.check_planet(planet_zones, testing=True) == saturn)
+    unittest(rocket.check_planet(planet_zones, testing=True) == uranus)
+    unittest(rocket.check_planet(planet_zones, testing=True) == neptune)
+
 
     unittest(rocket.check_planet(planet_zones) == None)
 
