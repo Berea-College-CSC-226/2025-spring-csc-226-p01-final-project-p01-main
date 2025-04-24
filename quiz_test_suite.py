79413c47fdd74294c18921b96cded8d9b7d2c9f2
baobao1355 <hoangb@berea.edu>
2025-04-23 20:11:44 -0400

    Finished the test suite

--- quiz_test_suite.py
+++ quiz_test_suite.py
@@ -1,39 +1,52 @@
-import unittest
 from inspect import getframeinfo, stack
-
-from data import question_data
-from quizbrain import *
-from questions import *
-
-import sys
-
-
+from data import fetch_questions
+from quizbrain import QuizBrain
+from questions import Question
 
 def unittest(did_pass):
     caller = getframeinfo(stack()[1][0])
     linenum = caller.lineno
     if did_pass:
-        msg = "Test at line {0} ok.".format(linenum)
+        print(f"Test at line {linenum} ok.")
     else:
-        msg = ("Test at line {0} FAILED.".format(linenum))
-    print(msg)
-
+        print(f"Test at line {linenum} FAILED.")
 
 def QuizBrain_suite():
-    quiz = QuizBrain(question_data)
+    # Fetch 5 questions for testing
+    raw_questions = fetch_questions(5, 9, "easy")  # Category 9 = General Knowledge
 
-    ##########################################
+    # Convert to Question objects
+    question_list = [
+        Question(q["question"], q["incorrect_answers"] + [q["correct_answer"]], q["correct_answer"])
+        for q in raw_questions
+    ]
+
+    quiz = QuizBrain(question_list)
+
     # Test still_has_question
     unittest(quiz.still_has_question() == True)
 
+    # Test next_question and check_answer
+    q_text, options = quiz.next_question()
+    unittest(isinstance(q_text, str))
+    unittest(isinstance(options, list))
 
-    ##########################################
+    correct_answer = quiz.current_question.answer
+    unittest(quiz.check_answer(correct_answer) == True)
+    unittest(quiz.score == 1)
 
+    # Test incorrect answer
+    quiz.next_question()
+    unittest(quiz.check_answer("WRONG_ANSWER") == False)
+
+    # Test finishing the quiz
+    while quiz.still_has_question():
+        quiz.next_question()
+
+    unittest(quiz.still_has_question() == False)
 
 def main():
     QuizBrain_suite()
 
-
 if __name__ == "__main__":
     main()
-
